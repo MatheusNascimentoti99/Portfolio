7b2919d33c7c2f25c4d049125365443fb4645e68
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2024-12-04 20:17:18 -0300

    fix: Define dark auto

--- quasar.config.ts
+++ quasar.config.ts
@@ -100,7 +100,9 @@ export default defineConfig((ctx) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        dark: 'auto'
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
