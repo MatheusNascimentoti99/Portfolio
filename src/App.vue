a6e421234ea3816d0868274858b8bb010d625a1f
matheusjus <matheus.plima@trf1.jus.br>
2024-12-04 11:41:35 -0300

    feat: Add SpeedInsights

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <SpeedInsights />
   <router-view />
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 defineOptions({
-  name: 'App'
+  name: 'App',
 });
 </script>
